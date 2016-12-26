22218c3d3983356f053badeb88f0f884833916df
Carlos Escobar <carlos.b.escobar@gmail.com>
2016-12-25 22:49:14 -0500

    adding page components

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>Pluralsight Administration</h1>
+                <p>react and something blah, blah, blah</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+            </div>
+        );
+    }
+
+
+}
+
+export default HomePage;

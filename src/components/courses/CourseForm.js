263d6da3178c9ec8c70d0ef19fd5a536dd8388df
Carlos Escobar <carlos.b.escobar@gmail.com>
2016-12-29 14:51:18 -0500

    adding course form

---
+++ src/components/courses/CourseForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import TextInput from '../common/TextInput';
+import SelectInput from '../common/SelectInput';
+
+const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => {
+    return (
+        <form>
+            <TextInput
+                name="title"
+                label="Title"
+                value={course.title}
+                onChange={onChange}
+                error={errors.title}
+            />
+            <SelectInput
+                name="authorId"
+                label="Author"
+                value={course.authorId}
+                onChange={onChange}
+                defaultOption="Select Author"
+                options={allAuthors}
+                error={errors.authorId}
+            />
+            <TextInput
+                name="category"
+                label="Category"
+                value={course.category}
+                onChange={onChange}
+                error={errors.category}
+            />
+            <TextInput
+                name="length"
+                label="Length"
+                value={course.length}
+                onChange={onChange}
+                error={errors.length}
+            />
+
+            <input
+                type="submit"
+                disabled={loading}
+                value={loading ? "Saving..." : "Save"}
+                className="btn btn-primary"
+                onClick={onSave}
+            />
+
+        </form>
+    );
+};
+
+CourseForm.propTypes = {
+    course: React.PropTypes.object.isRequired,
+    allAuthors: React.PropTypes.array,
+    onSave: React.PropTypes.func.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    loading: React.PropTypes.bool,
+    errors: React.propTypes.object
+};
+
+export default CourseForm;

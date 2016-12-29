c1acb06cdc3a35e80ca699831e620fffca3d07ed
Carlos Escobar <carlos.b.escobar@gmail.com>
2016-12-29 12:53:19 -0500

    adding mock api, adding course list component, adding course row component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import {render} from 'react-dom';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
+import {loadCourses} from './actions/courseActions';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
     <Provider store={store}>
